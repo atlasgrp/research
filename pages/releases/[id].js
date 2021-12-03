d6fb18d002272fee3ebb86a4854cb597ed69e3bc
hobbleabbas <shakeel@atlasfinance.org>
2021-12-03 11:21:36 -0500

    build error

--- pages/releases/[id].js
+++ pages/releases/[id].js
@@ -97,7 +97,7 @@ export default function Article() {
                             </h1>
                             <div className = 'mt-4 flex items-center justify-center mb-4'>
                                 <Link href = {article.link}>
-                                    <button type="button" class="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                    <button type="button" className="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                         Read on GlobeNewsWire
                                         <ExternalLinkIcon className="ml-2 w-5 h-5 -mr-1" />
                                     </button>
