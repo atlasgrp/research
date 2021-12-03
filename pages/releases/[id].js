39751bfad9b7e7e02fda3fcd0abf7224a9deb368
hobbleabbas <shakeel@atlasfinance.org>
2021-12-03 11:19:57 -0500

    build error and changes to article page

--- pages/releases/[id].js
+++ pages/releases/[id].js
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import { supabase } from '../../lib/supabaseClient';
 import Loading from '../../components/Root/Loading';
 import Link from 'next/link';
+import { ExternalLinkIcon } from '@heroicons/react/outline';
 
 export default function Article() {
     
@@ -14,19 +15,24 @@ export default function Article() {
     const router = useRouter();
     const { id } = router.query;
 
-    const fetchArticle = async () => {
-
+    const fetchId = async () => {
         const { id } = router.query;
 
+        return id
+    }
+
+    const fetchArticle = async () => {
+
+        const id = await fetchId();
+
         const { data, error } = await supabase.from('releases').select(`
-            type,
-            title,
-            ticker_1 (
-                id,
-                ticker
-            ),
-            body
-        `).eq('id', id)
+            contributor,
+            subject,
+            articleText,
+            link,
+            modified,
+            title
+        `).eq('identifier', id ?? 1)
         
         if (error) {
             alert(error.message)
@@ -39,7 +45,7 @@ export default function Article() {
 
     useEffect(() => {  
         fetchArticle()
-    }, [])
+    }, [id])
     
     return (
         <div>
@@ -56,7 +62,7 @@ export default function Article() {
                         <div className="relative px-4 sm:px-6 lg:px-8">
                         <div className="text-lg max-w-prose mx-auto">
                             <h1>
-                            <div className = 'flex items-center justify-center mb-4'>
+                            {/* <div className = 'flex items-center justify-center mb-4'>
                                 <Link href = {"/companies/" + article.ticker_1.ticker}>
                                     <a className="border cursor-pointer inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-white text-green-700 border-green-600">
                                         <svg className="-ml-1 mr-1.5 h-2 w-2 text-green-600" fill="currentColor" viewBox="0 0 8 8">
@@ -65,20 +71,44 @@ export default function Article() {
                                         {article.ticker_1.ticker}
                                     </a>
                                 </Link>
+                            </div> */}
+                            <div className = 'flex items-center justify-center mb-4'>
+                                <div className="border inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-white text-green-700 border-green-600">
+                                    <svg className="-ml-1 mr-1.5 h-2 w-2 text-green-600" fill="currentColor" viewBox="0 0 8 8">
+                                    <circle cx={4} cy={4} r={3} />
+                                    </svg>
+                                    {article.subject}
+                                </div>
+                                <div className = 'm-2'></div>
+                                <div className="border inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-white text-blue-700 border-blue-600">
+                                    <svg className="-ml-1 mr-1.5 h-2 w-2 text-blue-600" fill="currentColor" viewBox="0 0 8 8">
+                                    <circle cx={4} cy={4} r={3} />
+                                    </svg>
+                                    {article.contributor}
+                                </div>
                             </div>
                             <span className="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">
-                                {article.type}
+                                {(new Date(Date.parse(article.modified))).toLocaleDateString()}
                             </span>
                             <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                                 {article.title}
                             </span>
+                               
                             </h1>
+                            <div className = 'mt-4 flex items-center justify-center mb-4'>
+                                <Link href = {article.link}>
+                                    <button type="button" class="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                        Read on GlobeNewsWire
+                                        <ExternalLinkIcon className="ml-2 w-5 h-5 -mr-1" />
+                                    </button>
+                                </Link>
+                            </div>
+                            
                             <p className="mt-8 text-xl text-gray-500 leading-8">
                                 {article.description}
                             </p>
                         </div>
-                        <div className="mt-6 px-12 text-lg text-gray-500 mx-auto">
-                            {article.body}
+                        <div className="mt-6 px-12 text-lg text-gray-500 mx-auto" dangerouslySetInnerHTML={{ __html: article.articleText }}>
                         </div>
                         </div>
                     </div>
--- pages/releases/index.js
+++ pages/releases/index.js
@@ -77,9 +77,8 @@ const Hits = ({ hits }) => (
                     </div>
                 </div>
                 <a 
-                    href = {hit.link} 
+                    href = {'/releases/' + hit.identifier} 
                     rel="noreferrer"
-                    target="_blank"
                     className="flex-shrink-0 cursor-pointer inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                     Read More &rarr;
@@ -106,9 +105,8 @@ const Hits = ({ hits }) => (
                     </div>
                 </div>
                 <a 
-                    href = {hit.link} 
+                    href = {'/releases/' + hit.identifier} 
                     rel="noreferrer"
-                    target="_blank"
                     className="flex-shrink-0 mt-2 cursor-pointer inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                     Read More &rarr;
