42903b21ef8ef022c7b3560dfb5aa44f5f81814c
hobbleabbas <shakeel@atlasfinance.org>
2021-12-04 16:32:01 -0500

    fixed pagination chevrons

--- pages/releases/index.js
+++ pages/releases/index.js
@@ -127,6 +127,9 @@ const Pagination = ({
 	  <ul  className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" >
 	  	<a
         href="#"
+        onClick={()=>{
+            refine(currentRefinement - 1)
+        }}
         className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
       >
         <span className="sr-only">Previous</span>
@@ -156,6 +159,9 @@ const Pagination = ({
         className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
       >
         <span className="sr-only">Next</span>
+        onClick={()=>{
+            refine(currentRefinement + 1)
+        }}
         <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
       </a>
 	  </ul>
