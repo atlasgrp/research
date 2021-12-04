e345ed61167db35bdcc62bd2566755b66eab65bb
hobbleabbas <shakeel@atlasfinance.org>
2021-12-04 16:33:22 -0500

    fixed left pagination button

--- pages/releases/index.js
+++ pages/releases/index.js
@@ -155,15 +155,15 @@ const Pagination = ({
 	      );
 	    })}
 	    <a
-        href="#"
-        className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-      >
-        <span className="sr-only">Next</span>
-        onClick={()=>{
-            refine(currentRefinement + 1)
-        }}
-        <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-      </a>
+            href="#"
+            onClick={()=>{
+                refine(currentRefinement + 1)
+            }}
+            className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+        >
+            <span className="sr-only">Next</span>
+            <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+        </a>
 	  </ul>
 );
 
